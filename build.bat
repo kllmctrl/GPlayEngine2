960be618308abd26cf3bc4370c9c158a3fa0fb63
kllmctrl <kllmctrl@gmail.com>
2021-06-20 14:35:13 +0800

    test win10

---
+++ build.bat
@@ -0,0 +1,5 @@
+cd BUILD
+
+cmake -G "Visual Studio 16 2019" -D CMAKE_BUILD_TYPE=Debug
+
+pause
--- thirdparty/cmake/msvc-amalgamate.bat
+++ thirdparty/cmake/msvc-amalgamate.bat
@@ -1,19 +1,14 @@
 set ROOTDIR=%CD%
-set LIBDIR=%1
-set OUTDIR=%2
+set LIBDIR=%2
+set OUTDIR=%3
 
-echo "Amalgamating target dir: %LIBDIR%"
-
+echo "Amalgamating00 target dir: %LIBDIR%"
 md "%LIBDIR%"
-
 cd %LIBDIR%
 del gplay-deps.lib
 md tmp
 xcopy *.lib tmp
-
 LIB.EXE /OUT:gplay-deps.lib tmp\*
-
 move gplay-deps.lib %OUTDIR%
-
 rmdir /s /q "tmp\"
 cd %ROOTDIR%
